2de1b42b6d172feb8630bd42349588cec564dfbc
Erika Wunderli-Vallai <vallai.erika@gmail.com>
2018-10-17 10:41:35 +0200

    Update pages.js

--- Vacation_Management/frontend/js/pages.js
+++ Vacation_Management/frontend/js/pages.js
@@ -1,4 +1,10 @@
+// Send a notification via email: 
+function sendMail() {
+   window.location.href = "mailto:user@example.com?subject=Subject&body=message%20goes%20here";
+}
+sendemail();
 
+// Log out:
 function logoutFunction () {
     $("logout").load("frontend/logout.html")
 };
@@ -11,20 +17,24 @@ function notifyFunction() {
     }
 };    
 
+//Delete a row from the table
 $(".delete-row").click(function(){
 $(this).parents("tr").remove();
 });
 
+//Display selected item 
  $(document).ready(function(){
     $('select').formSelect();
   });
-        
+
+//Modify changes
 $(document).ready(function(){
     $(".modify-button").click(function(){
         $("#001").replaceWith();
     });
 });
 
+// Datepicker from calendar
 $(document).ready(function($)
 {
 $(".dropdown-trigger").dropdown();
@@ -38,7 +48,6 @@ $(document).ready(function(){
      $('select').material_select();
      $('.dropdown-button').dropdown();
 
-   }); // end of document ready
+   });
   });
  })(jQuery);
-
